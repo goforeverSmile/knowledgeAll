442b0f7a8014782c0d7db903adc8ed87a40e2271
PC-20170719NJKE\Administrator <chengangbao@weile.com>
2018-10-12 17:36:02 +0800

    python安装包的安装方法

---
+++ python的如何安装库一些知识点.py
@@ -0,0 +1,8 @@
+#为了获取数据信息我需要对tushare下载
+#这里就用到pip 需要在 C:\Python27\Scripts 文件夹下才可以使用pip命令
+#pip install tushare 反馈要要升级pip
+#------------如何升级pip------
+# pip show pip 显示版本,拷贝一段百度知识:: Pip是一个Python的包管理工具，实际上它也可以被看待为是一个包，
+# Pip相当于Linux上的yum，对python的开发者来说相当方便。我们再也无需去焦头烂额的寻找whl包，直接通过pip就可
+# 以在线安装（前提是有网络+pip版本合适的情况下,可见pip版本的重要性
+python -m pip install --upgrade pip 升级pip
