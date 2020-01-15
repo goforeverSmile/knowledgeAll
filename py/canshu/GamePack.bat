81ca77a6d097ad8efe7df578439c1fa3938d846d
chengangbao <chengangbao@weile.com>
2020-01-15 15:54:45 +0800

    11

--- py/canshu/GamePack.bat
+++
@@ -1,6 +0,0 @@
-
- rem   python pack.py -g tjhd
-    python pack.py -g tjmj -v 6
- rem   python pack.py -g tjsg
-
-PAUSE
--- py/canshu/JIxiang_TianJin.bat
+++
@@ -1,7 +0,0 @@
-
-
-::天津
-rem python ../../../src/tools/hall/UpdateBuilder/UpdateBuilder_js.py -c ../config/TianJin/tjmj.json -v 5
-
-python packceshi.py -g hainan 
-pause
